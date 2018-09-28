06a9d303640d5cd86af69f157db63ba6f8eabc79
Kevin Moore <k.james.moore@gmail.com>
2018-09-27 17:17:36 -0700

    Clean up

--- user/signup.js
+++ user/signup.js
@@ -1,13 +1,9 @@
-// Create binding to the signup form DOM node
+//
+// save user info when they submit, then redirect to profile.
+//
 const signUpForm = document.forms.SignUp;
 
-// JavaScript runs an "event loop" that responds to
-// user actions ("interactivity").
-//
-// Attach "event listener" to the sign up form.
-//
-signUpForm.addEventListener("submit", function(event){
-
+signUpForm.addEventListener("submit", function(event) {
   // 1. stop the page from refreshing
   event.preventDefault();
 
@@ -15,14 +11,27 @@ signUpForm.addEventListener("submit", function(event){
   const name = signUpForm.elements.name.value;
   const jobTitle = signUpForm.elements.jobTitle.value;
 
-  // 3. "serialize" the data. It needs to ultimately
-  // be in a string because localStorage only accepts
-  // strings.
+  // 3. Use utility method to save user data
+  saveUserToLocalStorage(name, jobTitle);
+
+  // 4. Send the user off to their profile page
+  window.location.href = "/user/profile.html";
+});
+
+
+//
+// utility method
+//
+function saveUserToLocalStorage(name, jobTitle) {
   const userJSON = {
     "name": name,
     "jobTitle": jobTitle
   }
+
+  // 1. "Serialize" the data, i.e. convert it into the format
+  // your storage layer uses. localStorage will only store strings.
   const userStringified = JSON.stringify(userJSON);
 
+  // 2. Now save the serialized data to localStorage.
   localStorage.setItem("_cb_user", userStringified);
-});
+}
--- water/water.js
+++ water/water.js
@@ -1,45 +1,33 @@
-/* The following code:
-*  1. Creates a binding to the #WaterSources ordered list element.
-*  2. Using the DOM's Fetch API , send an HTTP GET Request to
-*     https://localhost:8000/data/water.json. Look at
-*     MDN's "Using Fetch" articles to learn more about this API.
-*  3. Upon successful receipt of the data, we convert the JSON into
-*     a JavaScript object (an array).
-*  4. Loop over the array. Send each individual water source object into
-*     a function that returns a DOM list item node.
-*  5. Attach that node to the DOM, using the binding we created in step 1.
-*/
-
-
-// binding
-const list = document.getElementById("WaterSources");
-
-
-// using the Fetch API to GET water.json,
-// then turning JSON response into a JavaScript object (array)
-// then looping over array and attaching data to DOM with utility method.
+// Makes HTTP request for JSON file
+// Then extracts JSON data from HTTP response
+// Then calls utility method 1, passing it the data.
 fetch("/data/water.json")
-  .then(function(response) {
-    // console.dir(response);
-    return response.json();
-  })
-  .then(function(sources) {
-    // console.log(typeof sources);
-    sources.forEach(
-      function(source){
-        // console.log(source);
-        let li = createListItem(source);
-        list.appendChild(li);
+    .then(function(response) {
+      return response.json();
     })
-  });
-  // Uncomment the console statements to explore in the JavaScript console.
+    .then(function(sources) {
+      addSourcesToPage(sources);
+    });
 
+// Utility method 1:
+// Iterates over water sources array, passing each
+// water source object to Utility method 2. Method 2 returns
+// `li` DOM nodes, which are then appended to the list we created
+// in water.html.
+function addSourcesToPage(sources) {
+    const list = document.getElementById("WaterSources");
 
-// Utility method that takes water source objects
-// and returns a DOM list item node.
-// Note that the final forEach loop above
+    for (let i = 0 ; i < sources.length; i++ ){
+        let li = createListItem(sources[i]);
+        list.appendChild(li);
+    }
+}
+
+// Utility method 2.
+// Takes an individual water source objects as an argument
+// Returns an `li` DOM node with the data inside.
 function createListItem(data) {
-  let listItem = document.createElement("li");
+  const listItem = document.createElement("li");
   listItem.innerHTML = data.name;
 
   let paragraph = document.createElement("p");
